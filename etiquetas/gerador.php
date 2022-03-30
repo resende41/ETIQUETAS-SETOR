ac6e606de7cfd6819ac82aa1351b3190fcd061c0
resende41 <jef.rsd.contato@gmail.com>
2022-03-30 12:11:05 -0300

    finish

--- etiquetas/gerador.php
+++ etiquetas/gerador.php
@@ -1,22 +1,23 @@
 <DOCTYPE html>
-<link rel="stylesheet" href="content/css/application.css">
-<link rel="stylesheet" href="content/css/form.css">
-<nav class="modulos">
-    <div class="modulo azul">
-        <h3>PRODUCT PRINTER</h3>
-        <ul>
-            <li>
-                <a>
-                    <form class="dados" action ="etiquetas/print.php" method="get">
-                        <p>CÓDIGO DO PRODUTO: <input type="text" name="cod" /></p>
-                        <p>QUANTIDADE: <input type="float" name="qntd" /></p>
-                        <p>OBSERVAÇÕES: <input type="text" name="obs" /></p>
-                        <p>OPERADOR: <input type="text" name="obs" /></p>
-                        <p><input type="submit" /></p>
-                    </form>
-                </a>
-            </li>
-        </ul>
-    </div>
-</nav>
+    <link rel="stylesheet" href="content/css/application.css">
+    <link rel="stylesheet" href="content/css/form.css">
+    <nav class="modulos">
+        <div class="modulo azul">
+            <h3>PRODUCT PRINTER</h3>
+            <ul>
+                <li>
+                    <a>
+                        <form class="dados" action="etiquetas/print.php" method="post">
+                            <p>CÓDIGO DO PRODUTO: <input id="cod" type="text" name="cod" /></p>
+                            <p>OP: <input id="op" type="text" name="op" /></p>
+                            <p>QUANTIDADE: <input id="qntd" type="float" name="qntd" /></p>
+                            <p>OBSERVAÇÕES: <input id="obs" type="text" name="obs" /></p>
+                            <p>OPERADOR: <input id="operator" type="text" name="operator" /></p>
+                            <p><input type="submit" /></p>
+                        </form>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </html>
--- etiquetas/print.php
+++ etiquetas/print.php
@@ -1,36 +1,124 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "Darth.Vader99@";
+$dbname = "aubicon";
+// Criar conexão
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$cod = $_POST['cod'];
+
+$sql = mysqli_query($conn, "SELECT * FROM itens WHERE (codigo LIKE '%" . $cod . "%')");
+
+$row = mysqli_num_rows($sql);
+
+// if ($row > 0) {
+//     while ($linha = mysqli_fetch_array($sql)) {
+//         $codigo = $linha['codigo'];
+//         echo "<br/>";
+//         echo @$codigo;
+//     }
+// }
+// else {
+//     echo "Código inválido";
+// }
+
+?>
 <DOCTYPE html>
-<nav class="modulos">
-    <div class="modulo azul">
-        <h3>PRODUCT PRINTER</h3>
-        <ul>
-            <li>
-                <a>
-                    <table border="1">
-                        <thead>
-                            <tr>
-                                <th>&nbsp;</th>
-                                <th>Item</th>
-                                <th>Descrição</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>&nbsp;</th>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <th>QNTD</td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </a>
-            </li>
-        </ul>
-    </div>
-</nav>
+    <br>
+    <table border="1">
+        <tbody style="font-family: 'Oswald', sans-serif; font-size: 12px";>
+            <tr>
+                <th>ITEM</th>
+                <td>
+                    <?php
+                    $cod = $_POST['cod'];
+                    if (empty($cod)) {
+                        echo "Vazio";
+                    } else {
+                        echo $cod;
+                    }
+                    ?>
+                </td>
+                <th>DESCRIÇÃO</th>
+            </tr>
+            <tr>
+                <th>OP</th>
+                <td>
+                    <?php
+                    $op = $_POST['op'];
+                    if (empty($op)) {
+                        echo "Vazio";
+                    } else {
+                        echo $op;
+                    }
+                    ?>
+                </td>
+                <td style="text-align:center; font-size:10px" rowspan="4">
+                    <?php
+                        if ($row > 0) {
+                            while ($linha = mysqli_fetch_array($sql)) {
+                                $descricao = $linha['descricao'];
+                                echo "<br/>";
+                                echo @$descricao;
+                            }
+                        }
+                        else {
+                            echo "Código inválido";
+                        }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <th>m²</td>
+                <td>
+                    <?php
+                    $qntd = $_POST['qntd'];
+                    if (empty($qntd)) {
+                        echo "Vazio";
+                    } else {
+                        echo $qntd;
+                    }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <th style="font-size: 12px;">OPERADOR</td>
+                <td>
+                    <?php
+                    $operator = $_POST['operator'];
+                    if (empty($operator)) {
+                        echo "Vazio";
+                    } else {
+                        echo $operator;
+                    }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <th style="font-size: 12px;">
+                    <?php
+                        $originalDate = date('D, M, Y');
+                        //original date is in format YYYY-mm-dd
+                        $timestamp = strtotime($originalDate); 
+                        $newDate = date("d-m-Y", $timestamp );
+                        echo "$newDate";
+                    ?>
+                </th>
+                <td>
+                    <?php
+                    $obs = $_POST['obs'];
+                    if (empty($obs)) {
+                        echo "Vazio";
+                    } else {
+                        echo $obs;
+                    }
+                    ?>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </html>
